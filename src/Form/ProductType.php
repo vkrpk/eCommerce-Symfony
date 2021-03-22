ac3890d0248f665dd30c935382009b0c2faa60b4
Vic <vic_-@live.fr>
2021-03-22 17:48:08 +0100

    Fonctionnalités avancées du composant Form

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
+
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProductType extends AbstractType
@@ -33,7 +34,8 @@ class ProductType extends AbstractType
                 'label' => 'prix du produit',
                 'attr' => [
                     'placeholder' => 'Tapez le prix du produit en €'
-                ]
+                ],
+                'divisor' => 100
             ])
             ->add('mainPicture', UrlType::class, [
                 'label' => 'Image du produit',
@@ -50,7 +52,41 @@ class ProductType extends AbstractType
                     return strtoupper($category->getName());
                 }
             ]);
+
+        // $builder->get('price')->addModelTransformer(new CentimesTransformer);
     }
+
+    // $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
+    //     $product = $event->getData();
+
+    //     if ($product->getPrice() !== null) {
+    //         $product->setPrice($product->getPrice() * 100);
+    //     }
+    // });
+
+    // $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+    //     $form = $event->getForm();
+
+    //     /** @var Product */
+    //     $product = $event->getData();
+
+    //     if ($product->getPrice() !== null) {
+    //         $product->setPrice($product->getPrice() / 100);
+    //     }
+
+    // if ($product->getId() === null) {
+    //     $form->add('category', EntityType::class, [
+    //         'label' => 'Catégorie',
+    //         'attr' => [],
+    //         'placeholder' => '-- Choisir une catégorie --',
+    //         'class' => Category::class,
+    //         'choice_label' => function (Category $category) {
+    //             return strtoupper($category->getName());
+    //         }
+    //     ]);
+    // }
+    // });
+
 
     public function configureOptions(OptionsResolver $resolver)
     {
